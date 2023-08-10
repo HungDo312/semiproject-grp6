2697386c4071d2ed3996cf7fa292ea7351c8d67f
HungDo312 <hungdmbh00061@gmail.com>
2023-08-10 19:13:20 +0700

    Create table user and admin
    Create table user and admin

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes(['register'=>false]);
+
+Route::get('user/login','FrontendController@login')->name('login.form');
+Route::post('user/login','FrontendController@loginSubmit')->name('login.submit');
+Route::get('user/logout','FrontendController@logout')->name('user.logout');
+
+Route::get('user/register','FrontendController@register')->name('register.form');
+Route::post('user/register','FrontendController@registerSubmit')->name('register.submit');
+// Reset password
+Route::post('password-reset', 'FrontendController@showResetForm')->name('password.reset');
+// Socialite
+Route::get('login/{provider}/', 'Auth\LoginController@redirect')->name('login.redirect');
+Route::get('login/{provider}/callback/', 'Auth\LoginController@Callback')->name('login.callback');
