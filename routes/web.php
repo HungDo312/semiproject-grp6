a440ccfba2f823e224d746de11c3e2c994d0b019
HungDo312 <hungdmbh00061@gmail.com>
2023-08-10 22:32:17 +0700

    Update web.php
    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Auth::routes(['register'=>false]);
+Auth::routes(['register' => false]);
 
-Route::get('user/login','FrontendController@login')->name('login.form');
-Route::post('user/login','FrontendController@loginSubmit')->name('login.submit');
-Route::get('user/logout','FrontendController@logout')->name('user.logout');
+Route::get('user/login', 'FrontendController@login')->name('login.form');
+Route::post('user/login', 'FrontendController@loginSubmit')->name('login.submit');
+Route::get('user/logout', 'FrontendController@logout')->name('user.logout');
 
-Route::get('user/register','FrontendController@register')->name('register.form');
-Route::post('user/register','FrontendController@registerSubmit')->name('register.submit');
+Route::get('user/register', 'FrontendController@register')->name('register.form');
+Route::post('user/register', 'FrontendController@registerSubmit')->name('register.submit');
 // Reset password
 Route::post('password-reset', 'FrontendController@showResetForm')->name('password.reset');
 // Socialite
 Route::get('login/{provider}/', 'Auth\LoginController@redirect')->name('login.redirect');
 Route::get('login/{provider}/callback/', 'Auth\LoginController@Callback')->name('login.callback');
 
-Route::get('/','FrontendController@home')->name('home');
+// Frontend Routes
+Route::get('/', 'FrontendController@home')->name('home');
+Route::get('/home', 'FrontendController@index');
+Route::get('/about-us', 'FrontendController@aboutUs')->name('about-us');
+Route::get('/contact', 'FrontendController@contact')->name('contact');
+Route::post('/contact/message', 'MessageController@store')->name('contact.store');
+Route::get('product-detail/{slug}', 'FrontendController@productDetail')->name('product-detail');
+Route::post('/product/search', 'FrontendController@productSearch')->name('product.search');
+Route::get('/product-cat/{slug}', 'FrontendController@productCat')->name('product-cat');
+Route::get('/product-sub-cat/{slug}/{sub_slug}', 'FrontendController@productSubCat')->name('product-sub-cat');
+Route::get('/product-brand/{slug}', 'FrontendController@productBrand')->name('product-brand');
