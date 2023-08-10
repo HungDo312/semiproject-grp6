fae09edca3e2439aff3ac3e4d7c12ad7ec306fa0
HungDo312 <125356880+MD2dm@users.noreply.github.com>
2023-08-10 23:18:39 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -44,19 +44,45 @@ Route::post('/add-to-cart', 'CartController@singleAddToCart')->name('single-add-
 Route::get('cart-delete/{id}', 'CartController@cartDelete')->name('cart-delete');
 Route::post('cart-update', 'CartController@cartUpdate')->name('cart.update');
 
-Route::get('/cart',function(){
+Route::get('/cart', function () {
     return view('frontend.pages.cart');
 })->name('cart');
-Route::get('/checkout','CartController@checkout')->name('checkout')->middleware('user');
-Route::get('/wishlist',function(){
+Route::get('/checkout', 'CartController@checkout')->name('checkout')->middleware('user');
+Route::get('/wishlist', function () {
     return view('frontend.pages.wishlist');
 })->name('wishlist');
-Route::get('/wishlist/{slug}','WishlistController@wishlist')->name('add-to-wishlist')->middleware('user');
-Route::get('wishlist-delete/{id}','WishlistController@wishlistDelete')->name('wishlist-delete');
+Route::get('/wishlist/{slug}', 'WishlistController@wishlist')->name('add-to-wishlist')->middleware('user');
+Route::get('wishlist-delete/{id}', 'WishlistController@wishlistDelete')->name('wishlist-delete');
 // Route::post('cart/order','OrderController@store')->name('cart.order');
-Route::post('cart/order','CartConTroller@postPay')->name('product.order');
-Route::get('order/pdf/{id}','OrderController@pdf')->name('order.pdf');
-Route::get('/income','OrderController@incomeChart')->name('product.order.income');
+Route::post('cart/order', 'CartConTroller@postPay')->name('product.order');
+Route::get('order/pdf/{id}', 'OrderController@pdf')->name('order.pdf');
+Route::get('/income', 'OrderController@incomeChart')->name('product.order.income');
 // Route::get('/user/chart','AdminController@userPieChart')->name('user.piechart');
-Route::get('/product-grids','FrontendController@productGrids')->name('product-grids');
-Route::get('/product-lists','FrontendController@productLists')->name('product-lists');
+Route::get('/product-grids', 'FrontendController@productGrids')->name('product-grids');
+Route::get('/product-lists', 'FrontendController@productLists')->name('product-lists');
+Route::get('/product/track', 'OrderController@orderTrack')->name('order.track');
+Route::post('product/track/order', 'OrderController@productTrackOrder')->name('product.track.order');
+// Blog
+Route::get('/blog', 'FrontendController@blog')->name('blog');
+Route::get('/blog-detail/{slug}', 'FrontendController@blogDetail')->name('blog.detail');
+Route::get('/blog/search', 'FrontendController@blogSearch')->name('blog.search');
+Route::post('/blog/filter', 'FrontendController@blogFilter')->name('blog.filter');
+Route::get('blog-cat/{slug}', 'FrontendController@blogByCategory')->name('blog.category');
+Route::get('blog-tag/{slug}', 'FrontendController@blogByTag')->name('blog.tag');
+
+// NewsLetter
+Route::post('/subscribe', 'FrontendController@subscribe')->name('subscribe');
+
+// Product Review
+Route::resource('/review', 'ProductReviewController');
+Route::post('product/{slug}/review', 'ProductReviewController@store')->name('review.store');
+
+// Post Comment
+Route::post('post/{slug}/comment', 'PostCommentController@store')->name('post-comment.store')->middleware('user');
+Route::resource('/comment', 'PostCommentController');
+// Coupon
+Route::post('/coupon-store', 'CouponController@couponStore')->name('coupon-store');
+// Payment
+Route::get('payment', 'PayPalController@payment')->name('payment');
+Route::get('cancel', 'PayPalController@cancel')->name('payment.cancel');
+Route::get('payment/success', 'PayPalController@success')->name('payment.success');
